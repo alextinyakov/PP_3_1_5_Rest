61d762cf60a481e91449953ea7b11cb70d68e3b1
ATiny <aleks_tii@mail.ru>
2023-01-05 17:48:54 +0300

    добавил admins_page и show_all_users

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,17 +1,29 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.Service.UserService;
 
 @Controller
 @RequestMapping("/")
 public class AdminController {
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/admins_page")
     public String home() {
 
         return "admins_page";
+    }
+    @GetMapping("/show_all")
+    public String ShowAll(ModelMap model) {
+        model.addAttribute("userslist", userService.index());
+        return "allUsers";
     }
 
 }
