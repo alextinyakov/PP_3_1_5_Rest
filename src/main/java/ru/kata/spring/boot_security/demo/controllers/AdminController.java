c39ce01ad4f70b7cf14b2670c2bdf792aa804ff3
ATiny <aleks_tii@mail.ru>
2023-01-17 19:58:50 +0300

    Исправления по замечаниям ментора

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
 import ru.kata.spring.boot_security.demo.models.Role;
@@ -19,12 +18,10 @@ import java.util.List;
 public class AdminController {
     private final UserService userService;
     private final RoleService roleService;
-    private final RoleRepository roleRepository;
 
-    public AdminController(UserService userService, RoleService roleService, RoleRepository roleRepository) {
+    public AdminController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
-        this.roleRepository = roleRepository;
     }
 
     @GetMapping("/admins_page")
@@ -52,7 +49,7 @@ public class AdminController {
     @GetMapping("/addUser")
     public String addUser(Model model) {
         model.addAttribute("user", new User());
-        model.addAttribute("role", roleRepository.findAll());
+        model.addAttribute("role", roleService.getAllRoles());
         return "new";
     }
 
